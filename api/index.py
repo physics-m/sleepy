08fc69bcd3864047cc886f495d85fca401fb2b52
Mr_Night_M <mapengsen@qq.com>
2026-06-30 02:49:38 +0800

    fix

--- api/index.py
+++ api/index.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
 from main import app
 
 application = app
